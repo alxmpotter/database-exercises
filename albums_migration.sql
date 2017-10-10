a31e7f4b85bdcc9ab2626a61c1ecc3c1e7ed5eaa
alxmpotter <alxmpotter@gmail.com>
2017-10-10 11:21:08 -0500

    mysql tables exercise
    *reformat table
    *finish exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,13 +2,13 @@ USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
---columns: ID, artist, name, release dates, sales, genre
+
 CREATE TABLE albums (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(100) NOT NULL,
-  record VARCHAR(200),
+  id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist       VARCHAR(100) NOT NULL,
+  record       VARCHAR(200),
   release_date DATE,
-  sales FLOAT,
-  genre VARCHAR(50),
+  sales        FLOAT,
+  genre        VARCHAR(50),
   PRIMARY KEY (id)
 );
