0013cb4584ca2a5298fb940863be5cf6da8146ba
alxmpotter <alxmpotter@gmail.com>
2017-10-10 14:28:42 -0500

    mysql insert exercise
    *add insert and values to albums table in seeder file
    *forgot to commit step a, can use multiple insert statements

---
+++ albums_seeder.sql
@@ -0,0 +1,23 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, record, release_date, sales, genre)
+VALUES
+  ('Michael Jackson', 'Thriller', '1982', '47.3', 'Pop, R&B'),
+  ('Fleetwood Mac', 'Rumours', '1977', '29.9', 'Soft rock'),
+  ('Eagles', 'Their Greatest Hits', '1976', '32.2', 'Rock, Soft rock, Folk rock'),
+  ('Whitney Houston', 'The Bodyguard', '1992', '27.4', 'R&B'),
+  ('AC/DC', 'Back in Black', '1980', '26.1', 'Hard rock'),
+  ('Pink Floyd', 'The Dark Side of the Moon', '1973', '24.2', 'Progressive rock'),
+  ('Meat Loaf', 'Bat Out of Hell', '1977', '21.5', 'Hard rock, Progressive rock'),
+  ('Bee Gees', 'Saturday Night Fever', '1977', '20.6', 'Disco'),
+  ('Shania Twain', 'Come On Over',	'1997',	'29.6', 'Country, Pop'),
+  ('Led Zeppelin',	'Led Zeppelin IV',	'1971', '29.0', 'Hard rock, Heavy metal, Folk'),
+  ('Micheal Jackson', 'Bad', '1987', '23.6', 'Pop, Funk, Rock'),
+  ('Alanis Morissette', 'Jagged Little Pill', '1995', '24.4', 'Alternative rock'),
+  ('Celine Dion',	'Falling into You',	'1996', '20.2', 'Pop, Soft rock'),
+  ('The Beatles	Sgt. Pepper''s', 'Lonely Hearts Club Band',	'1967', '13.1',	'Rock'),
+  ('Michael Jackson',	'Dangerous', '1991',	'19.4', 'Rock, Funk, Pop'),
+  ('Eagles', 'Hotel California', '1976', '21.5', 'Rock, Folk rock'),
+  ('Adele', '21', '2011', '17.9', 'Pop, Soul'),
+  ('Celine Dion', 'Let''s Talk About Love', '1997', '17.9', 'Pop, Soft rock'),
+  ('The Beatles', '1', '2000', '19.3', 'Rock');
