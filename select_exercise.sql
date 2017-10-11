f144a90c1f27a6a909853a8c450172d2bc39aef6
alxmpotter <alxmpotter@gmail.com>
2017-10-11 10:52:36 -0500

    select exercise
    *complete third task

--- select_exercise.sql
+++ select_exercise.sql
@@ -12,11 +12,16 @@ Which albums had less than 20 million certified sales
 
 All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?*/
 
-/*columns: artist, record, release_date, sales, genre*/
+-- columns: artist, record, release_date, sales, genre
 
+SELECT 'All albums by Pink Floyd:' AS '';
 SELECT record FROM albums WHERE artist = 'Pink Floyd';
 
+SELECT 'The year Sgt. Pepper''s was released:' AS '';
 SELECT release_date FROM albums WHERE record = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT 'The genre of Nirvana''s album Nevermind:' AS '';
+SELECT genre FROM albums WHERE record = 'Nevermind';
 
 
 
