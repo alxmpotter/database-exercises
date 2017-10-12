0c3d28b35b3dd82424d87e8bae69848b1bf3f87f
alxmpotter <alxmpotter@gmail.com>
2017-10-12 14:18:08 -0500

    order by exercise
    * complete all tasks

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -6,22 +6,32 @@ FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY last_name, first_name;
 
+-- next two queries produce same result, one using order by
 SELECT emp_no, last_name
 FROM employees
-WHERE last_name LIKE 'E%'
-      OR last_name LIKE '%E';
+WHERE last_name LIKE '%E%';
 
 SELECT emp_no, last_name
 FROM employees
 WHERE last_name LIKE '%E%'
 ORDER BY emp_no;
 
-SELECT emp_no, hire_date, last_name, birth_date
+-- reverse order
+SELECT emp_no, last_name
+FROM employees
+WHERE last_name LIKE '%E%'
+ORDER BY emp_no DESC;
+
+-- reverse order of first query, but same results?
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name DESC;
+
+-- change query for oldest employee who was hired last, result = Khun Bernini
+SELECT first_name, last_name
 FROM employees
 WHERE hire_date LIKE '199%'
-      AND (birth_date LIKE '%-12-25');
+      AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC;
 
-SELECT last_name
-FROM employees
-WHERE last_name like '%q%'
-      AND last_name NOT LIKE '%qu%';
