173f3a8f60f38a6d03b3b5891361909d6885d78a
alxmpotter <alxmpotter@gmail.com>
2017-10-12 10:59:58 -0500

    where  continued exercise
    *add query to find employees whose last name starts and ends with e
    
    *change between comparison to like comparison with a wild card for hire date query

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,5 +1,6 @@
 USE employees;
 
+
 SELECT first_name, last_name, gender
 FROM employees
 WHERE gender = 'M'
@@ -15,11 +16,16 @@ WHERE last_name LIKE 'E%'
 SELECT emp_no, last_name
 FROM employees
 WHERE last_name LIKE 'E%'
+      OR last_name LIKE '%E';
+
+SELECT emp_no, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
       AND last_name LIKE '%E';
 
 SELECT emp_no, hire_date, last_name, birth_date
 FROM employees
-WHERE hire_date BETWEEN '1990-1-1' AND '1999-12-31'
+WHERE hire_date LIKE '199%'
   AND (birth_date LIKE '%-12-25');
 
 
@@ -27,4 +33,4 @@ WHERE hire_date BETWEEN '1990-1-1' AND '1999-12-31'
 SELECT last_name
 FROM employees
 WHERE last_name like '%q%'
-      AND (last_name NOT LIKE '%qu%');
+      AND last_name NOT LIKE '%qu%';
